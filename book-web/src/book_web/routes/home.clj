1473095fdef823fa529ee976b88ed5b8297e15c6
leon xia <xiafei114@gmail.com>
2017-04-27 15:39:04 +0800

    增加提交

--- book-web/src/book_web/routes/home.clj
+++ book-web/src/book_web/routes/home.clj
@@ -1,9 +1,54 @@
 (ns book-web.routes.home
   (:require [compojure.core :refer :all]
-            [book-web.views.layout :as layout]))
+            [book-web.views.layout :as layout]
+            [hiccup.form :refer :all]))
 
-(defn home []
-  (layout/common [:h1 "Hello World!"]))
+
+(defn show-guests []
+  [:ul.guests
+   (for [{:keys [message name timestamp]}
+         [{:message "Howdy" :name "Bob" :timestamp nil}
+          {:message "Hello" :name "Bob" :timestamp nil}]
+         ]
+		   [:li
+      [:blockquote message]
+      [:p "-" [:cite name]]
+      [:time timestamp]]
+     )])
+
+
+(defn home [& [name message error]]
+  (layout/common
+    [:h1 "Guestbook"]
+    [:p "Welcome to my guestbook"]
+    [:p error]
+    ;here we call our show-guests function
+    ;to generate the list of existing comments
+  (show-guests)
+  [:hr]
+  ;here we create a form with text fields called "name" and "message"
+  ;these will be sent when the form posts to the server as keywords of
+  ;the same name
+  (form-to [:post "/"]
+           [:p "Name:"]
+           (text-field "name" name)
+           [:p "Message:"]
+           (text-area {:rows 10 :cols 40} "message" message)
+           [:br]
+           (submit-button "comment"))))
+
+
+(defn save-message [name message]
+  (cond
+    (empty? name)
+    (home name message "name 是空")
+    (empty? message)
+    (home name message "message 是空")
+    :else
+    (do
+      (println name message)
+      (home))))
 
 (defroutes home-routes
-  (GET "/" [] (home)))
+  (GET "/" [] (home))
+  (POST "/" [name message] (save-message name message)))
